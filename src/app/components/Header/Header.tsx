547b2ee66bd8f90c081b919a62e3bf6c7806a2a9
ayeletfridman <ayfridma@g.jct.ac.il>
2025-10-21 11:14:42 +0300

    fix the header design

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import styles from "./Header.module.css";
 const Header: React.FC = () => {
   return (
     <header className={styles.header}>
-      <h1>🛍️ חנות אונליין</h1>
+      <h1> חנות אונליין</h1>
       <div className={styles.logoContainer}>
         <img src="/logo.webp" alt="הדסים" className={styles.logo} />
       </div>
